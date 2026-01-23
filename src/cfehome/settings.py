9fa32d3f884a84873100ce6489ae50b1c942985d
mouhib539 <130019916+mouhib539@users.noreply.github.com>
2026-01-24 00:13:07 +0100

    setting up the env

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xqq$+cbs-f)1p*czleg%sik)f03dr*p*t)+ms^f(_g-bwqtih9'
+SECRET_KEY = "DJANGO_SECRET_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
+print("DEBUG =", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = ['.railway.app', '127.0.0.1', 'localhost']
 
